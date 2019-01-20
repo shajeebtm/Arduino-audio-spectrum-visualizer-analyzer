260629214eddd1436b925003f33535e253e67c6e
Shajeeb T Muhammad <shajeeb@outlook.com>
2019-01-19 21:22:35 -0800

    Update Arduino-32band-audio-spectrum-visualizer-analyzer.ino

--- Code/Arduino-32band-audio-spectrum-visualizer-analyzer.ino
+++ Code/Arduino-32band-audio-spectrum-visualizer-analyzer.ino
@@ -1,6 +1,9 @@
 /*
 Copyright (c) 2019 Shajeeb TM
 
+https://github.com/shajeebtm/Arduino-audio-spectrum-visualizer-analyzer/
+https://create.arduino.cc/projecthub/Shajeeb/32-band-audio-spectrum-visualizer-analyzer-902f51
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
